cfb8a0878ae577a48503e75d468f484570645e78
Денис Ядерцов <neelonmusk@MacBook-Pro-Denis-2.local>
2019-10-01 10:57:22 +0300

    1

--- 1/main.c
+++ 1/main.c
@@ -2,5 +2,18 @@
 
 int main(void)
 {
+    int n, mn, mx, data;
+    scanf("%d", &n);
+    scanf("%d", &mn);
+    mx = mn;
+    for (int i = 1; i < n; i++)
+    {
+        scanf("%d", &data);
+        if (data < mn)
+            mn = data;
+        if (data > mx)
+            mx = data;
+    }
+    printf("%d", mx - mn);
     return 0;
 }
